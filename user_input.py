3fdab3d05ab9cd514e1ccd475cac79658aabb6dd
thuku-tmn <thukutmn@gmail.com>
2024-03-02 15:45:25 +0300

    python first assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+Name=input("What is your Name? ")
+Age=int(input("What is your Age? "))
+Location=input("What is your Location? ")
+
+message=(f"Hello {Name} ,you are {Age} years old and live in {Location}")
+
+print(message)
